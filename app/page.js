a083b0e8390ffef6f11035633419b6546104d07b
prabuddha610 <prabuddha610@gmail.com>
2025-07-21 23:58:17 +0530

    Fixed errors in filenames

--- app/page.js
+++ app/page.js
@@ -3,8 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      <h1>Hello</h1>
-      <Button>Click me</Button>
+      
     </div>
   );
 }
