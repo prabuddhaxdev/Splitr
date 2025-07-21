f792515536be205f6a53598b4b2404f868e5b905
prabuddha610 <prabuddha610@gmail.com>
2025-07-21 21:58:02 +0530

    Add Users Table in DB and connected Convex with Clerk

--- components/convex-client-provider.jsx
+++ components/convex-client-provider.jsx
@@ -1,14 +1,13 @@
 "use client";
 
-import { ConvexReactClient } from "convex/react";
-import { ConvexProviderWithClerk } from "convex/react-clerk";
-import { useAuth } from "@clerk/nextjs";
+import { ConvexReactClient } from 'convex/react';
+import { ConvexProviderWithClerk } from 'convex/react-clerk';
+import { useAuth } from '@clerk/nextjs';
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL);
 
 export function ConvexClientProvider({ children }) {
-  return (<ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+  return <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
     {children}
-  </ConvexProviderWithClerk>
-  )
+  </ConvexProviderWithClerk>;
 }
