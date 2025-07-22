da8b121d092a773e4acdc0280d71ef3e44609f98
prabuddha610 <prabuddha610@gmail.com>
2025-07-23 00:08:38 +0530

    Add Custom Hooks for Fetching Data

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,11 @@
+import { Inter } from "next/font/google";
 import "./globals.css";
-import {Inter} from "next/font/google";
-import Header from "@/components/header";
-import { ConvexClientProvider } from "@/components/convex-client-provider";
 import { ClerkProvider } from "@clerk/nextjs";
+import { ConvexClientProvider } from "@/components/convex-client-provider";
+import Header from "@/components/header";
+import { Toaster } from "sonner";
 
-const inter = Inter({subsets: ["latin"]});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Splitr",
@@ -15,13 +16,19 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/logos/logo-s.png" />
+        <link rel="icon" href="/logos/logo-s.png" sizes="any" />
       </head>
       <body className={`${inter.className}`}>
-        <ClerkProvider>
+        <ClerkProvider
+          publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+        >
           <ConvexClientProvider>
             <Header />
-            <main className="min-h-screen">{children}</main>
+            <main className="min-h-screen">
+              <Toaster richColors />
+
+              {children}
+            </main>
           </ConvexClientProvider>
         </ClerkProvider>
       </body>
