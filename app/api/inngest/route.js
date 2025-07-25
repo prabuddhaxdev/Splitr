a7dfacfb2754783bca7b59775e3e0baf137308cd
prabuddha610 <prabuddha610@gmail.com>
2025-07-25 19:59:15 +0530

    Add Resend to Send Emails

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,5 +1,5 @@
 import { inngest } from "@/lib/inngest/client";
-import { helloWorld } from "@/lib/inngest/payment-reminders";
+import { paymentReminders } from "@/lib/inngest/payment-reminders";
 import { serve } from "inngest/next";
 
 // Create an API that serves zero functions
@@ -7,6 +7,6 @@ export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
     /* your functions will be passed here later! */
-    helloWorld,
+    paymentReminders,
   ],
 });
