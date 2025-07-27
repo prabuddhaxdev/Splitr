e083a3f6227694c46f616989ec01426fa44811a4
prabuddha610 <prabuddha610@gmail.com>
2025-07-27 23:44:14 +0530

    Add Favicon

--- app/layout.js
+++ app/layout.js
@@ -16,7 +16,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/logos/logo-s.png" sizes="any" />
+        <link rel="icon" href="/logos/logo-s.ico" sizes="any" />
       </head>
       <body className={`${inter.className}`}>
         <ClerkProvider
